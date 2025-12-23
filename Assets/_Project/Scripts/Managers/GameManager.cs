3f0f65128840692fc75aa5219f193040d68e0cb3
Ibrahim Dolek <idolek28@gmail.com>
2025-12-23 19:36:38 +0300

    feat: add gamemanager singleton and lose condition

---
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake() 
+    {
+        instance = this;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOver!!!!");
+    }
+}
