388759648d37499d94e1c48ba5b0713625b69d36
Ibrahim Dolek <idolek28@gmail.com>
2025-12-27 18:29:18 +0300

    feat: add basic highscore logic

--- Assets/_Project/Scripts/Managers/GameManager.cs
+++ Assets/_Project/Scripts/Managers/GameManager.cs
@@ -2,20 +2,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton yapısı
     public static GameManager instance;
 
     public int score = 0;
+    public int highScore;
 
     private void Awake() 
-    {
+    {   
         instance = this;
+    
+        score = 0;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        Debug.Log($"HighScore {highScore}");
     }
 
     public void IncreaseScore()
     {
         score++;
-
         Debug.Log($"Score: {score}");
+
+        if (score > highScore)
+        {
+            highScore = score;
+
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 
     public void GameOver()
